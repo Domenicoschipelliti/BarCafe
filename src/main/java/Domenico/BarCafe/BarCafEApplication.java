0044396c7c5e27480168e134fcdd7d5bf8bfc3b8
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2024-04-15 20:47:40 +0200

    edit2

--- src/main/java/Domenico/BarCafe/BarCafEApplication.java
+++ src/main/java/Domenico/BarCafe/BarCafEApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BarCafeApplication {
+public class BarCafEApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BarCafeApplication.class, args);
