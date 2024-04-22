b8ce8c87b910b5449c76944d465541ffb1472bf7
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2024-04-22 13:27:56 +0200

    bevande Service

--- src/main/java/Domenico/BarCafe/Service/BevandeService.java
+++ src/main/java/Domenico/BarCafe/Service/BevandeService.java
@@ -1,7 +1,44 @@
 package Domenico.BarCafe.Service;
 
+import Domenico.BarCafe.DAO.BevandeDAO;
+import Domenico.BarCafe.Enteties.Bevande;
+import Domenico.BarCafe.Exceptions.NotFound;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BevandeService {
+
+    @Autowired
+    private BevandeDAO bevandeDAO;
+
+
+    public List<Bevande> Alldrinks(){
+        return bevandeDAO.findAll();
+    }
+
+    public Bevande findByIdBevande(UUID bevandeId){
+        return bevandeDAO.findById(bevandeId).orElseThrow(()->new NotFound(bevandeId));
+    }
+
+
+    public Bevande bevandePut(UUID bevandeId,Bevande body){
+        Bevande bevande=this.findByIdBevande(bevandeId);
+        bevande.setCosto(body.getCosto());
+        bevande.setDescrizione(body.getDescrizione());
+        bevande.setUser(body.getUser());
+        bevande.setImmagine(body.getImmagine());
+        bevande.setNomeProdotto(bevande.getNomeProdotto());
+        return bevandeDAO.save(bevande);
+    }
+
+
+    public void bevandeDelete(UUID bevandeId){
+        Bevande bevande=this.findByIdBevande(bevandeId):
+        bevandeDAO.delete(bevande);
+    }
+
 }
