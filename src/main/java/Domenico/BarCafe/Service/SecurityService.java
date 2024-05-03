241b0eda14fae92642aba189e3775650035b45c0
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2024-05-03 17:24:27 +0200

    Update Security

--- src/main/java/Domenico/BarCafe/Service/SecurityService.java
+++ src/main/java/Domenico/BarCafe/Service/SecurityService.java
@@ -1,4 +1,28 @@
 package Domenico.BarCafe.Service;
 
+import Domenico.BarCafe.DAO.UtenteDAO;
+import Domenico.BarCafe.Security.Jwts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SecurityService {
+
+    @Autowired
+    private Jwts jwts;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UtenteService utenteService;
+
+    @Autowired
+    private UtenteDAO utenteDAO;
+
+
+
+
+
 }
