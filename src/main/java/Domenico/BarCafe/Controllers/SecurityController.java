54732ee2e772c704841d3ba7b81f5cbc5e83a1a8
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2024-05-11 18:42:32 +0200

    security service complete

---
+++ src/main/java/Domenico/BarCafe/Controllers/SecurityController.java
@@ -0,0 +1,41 @@
+package Domenico.BarCafe.Controllers;
+
+import Domenico.BarCafe.Enteties.Utente;
+import Domenico.BarCafe.Exceptions.BadRequest;
+import Domenico.BarCafe.Payload.UserDtoLogin;
+import Domenico.BarCafe.Payload.UserDtoLoginResponse;
+import Domenico.BarCafe.Payload.UserDtoRegister;
+import Domenico.BarCafe.Payload.UserDtoRegisterResponse;
+import Domenico.BarCafe.Service.SecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/security")
+public class SecurityController {
+    @Autowired
+    private SecurityService securityService;
+
+    @PostMapping("/login")
+    public UserDtoLoginResponse login(@RequestBody UserDtoLogin userDtoLogin){
+        String token=securityService.userResponse(userDtoLogin);
+        return new UserDtoLoginResponse(token);
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDtoRegisterResponse register(@RequestBody @Validated UserDtoRegister userDtoRegister, BindingResult validation){
+        if(validation.hasErrors()){
+            System.out.println(validation.hasErrors());
+            throw new BadRequest(validation.getAllErrors());
+
+        }else {
+            Utente nuovoUtente=securityService.saveUser(userDtoRegister);
+            return new UserDtoRegisterResponse(nuovoUtente.getUserId());
+        }
+
+    }
+}
